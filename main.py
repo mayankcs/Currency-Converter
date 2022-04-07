cd8012d506036d177f3d42ccf093c199256da248
MAYANK CHAUDHARI <65117236+mayankcs@users.noreply.github.com>
2022-04-07 21:44:40 +0530

    Changed the way of calling history

--- main.py
+++ main.py
@@ -11,26 +11,31 @@ amount=float(input("Amount to convert %s "%(source)))
 print("******************************")
 date=0
 
-try:
-    ex_rate,date=exchange_rate.get_exchange_rate(source,target)
-    history=history.get_history(source,target,date)
+
+ex_rate,date=exchange_rate.get_exchange_rate(source,target)
+
+result=amount*ex_rate
+print('\n')
+print('%s %d equals to %s %f'%(source,amount,target,result))
+
+#using history
+
+obj=history.history()
+history=obj.get_history(source,target,date)
+
+prevMonthRate=history[0][0] # accessing previous month date
+prevYearRate=history[1][0]# accessing previous year date
+
+#Converting user amount according to previous month and year
+prevMonthAmount=amount*prevMonthRate 
+prevYearAmount=amount*prevYearRate
 
 
-    result=amount*ex_rate
-    print('\n')
-    print('%s %d equals to %s %f'%(source,amount,target,result))
+print('\n')
+st1='last month %s %d was equals to %s %f'%(source,amount,target,prevMonthAmount)
+st2='last year %s %d was equals to %s %f'%(source,amount,target,prevYearAmount)
+print(st1)
+print(st2)
 
-    prevMonthRate=history[0][0]
-    prevYearRate=history[1][0]
 
-    prevMonthAmount=amount*prevMonthRate
-    prevYearAmount=amount*prevYearRate
-    print('\n')
-    st1='last month %s %d was equals to %s %f'%(source,amount,target,prevMonthAmount)
-    st2='last year %s %d was equals to %s %f'%(source,amount,target,prevYearAmount)
-    print(st1)
-    print(st2)
-
-except:
-    print("please type the country code correctly")
 
